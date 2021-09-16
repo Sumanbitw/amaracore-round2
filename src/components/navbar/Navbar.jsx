248fe28bc1935b3911f78d14707f96c66a9885c7
Sumanbitw <007sumanghosh@gmail.com>
2021-09-16 23:21:25 +0530

    refactoring styles of navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -12,20 +12,6 @@ function Navbar() {
     <section className="border-b-2 fixed w-full bg-white dark:bg-gray-900 text-gray-900 dark:text-white">
       <nav className="flex flex-wrap justify-between items-center h-auto p-4 z-50">
         <div className="flex justify-evenly items-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-8 w-8"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M4 6h16M4 12h16M4 18h16"
-            />
-          </svg>
           <img
             src="https://www.gstatic.com/images/branding/product/2x/contacts_48dp.png"
             alt=""

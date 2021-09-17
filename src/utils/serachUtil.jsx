ef3926c5e632107bc8574c9f68966a791f6b156b
Sumanbitw <007sumanghosh@gmail.com>
2021-09-17 15:56:42 +0530

    refactor

--- src/utils/serachUtil.jsx
+++
@@ -1,8 +0,0 @@
-
-
-export const searchContacts = (contacts, search) => {
-  console.log(contacts)
-  let filteredContacts = [...contacts]
-  filteredContacts = filteredContacts?.first_name.toLowerCase().includes(search?.toLowerCase())
-  return filteredContacts;
-};
